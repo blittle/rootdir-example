9164774770e4ed78cb9cce62cb81f0704debce1c
Bret Little <blittle@salesforce.com>
2020-03-27 13:47:34 -0400

    Fix it

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -1,6 +1,5 @@
 import lwc from '@lwc/rollup-plugin';
 import replace from 'rollup-plugin-replace';
-import resolve from 'rollup-plugin-node-resolve';
 import visualizer from 'rollup-plugin-visualizer';
 import path from 'path';
 
@@ -9,6 +8,11 @@ const input = path.resolve(__dirname, './src/index.js');
 const output = path.join(outputDir, 'app.js');
 const env = process.env.NODE_ENV || 'development';
 
+const node_modules = 'node_modules';
+const lwcPlugin = lwc({
+  rootDir: './src/modules',
+});
+
 export default {
   input,
   output: {
@@ -16,12 +20,21 @@ export default {
     file: path.join(outputDir, 'app.js'),
   },
   plugins: [
-    lwc({
-      rootDir: './src/modules',
-    }),
-    resolve({
-      dedupe: ['lwc'],
-    }),
+    {
+      resolveId(importee, importer) {
+        const result = lwcPlugin.resolveId(importee, importer);
+
+        if (importee === 'lwc') {
+          return require.resolve(
+            result.substring(
+              result.lastIndexOf(node_modules) + node_modules.length + 1,
+            ),
+          );
+        }
+        return result;
+      },
+    },
+    lwcPlugin,
     replace({'process.env.NODE_ENV': JSON.stringify(env)}),
     visualizer(),
   ],
